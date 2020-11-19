9446c5d8684adc85c202c7ae2eb9ac86cf276336
Simon Hochholzer <simon@symmetronic.com>
2020-11-19 12:16:02 +0100

    remove "Proudly powered by WP" note

--- footer.php
+++ footer.php
@@ -8,33 +8,26 @@
  * @subpackage Twenty_Fifteen
  * @since Twenty Fifteen 1.0
  */
+
+/**
+ * Modifications:
+ * 
+ * - Remove "Proudly powered by WordPress" note
+ * 
+ * Last modified 2020-11-19.
+ */
 ?>
 
 	</div><!-- .site-content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php
-				/**
-				 * Fires before the Twenty Fifteen footer text for footer customization.
-				 *
-				 * @since Twenty Fifteen 1.0
-				 */
-				do_action( 'twentyfifteen_credits' );
-			?>
-			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-			}
-			?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>" class="imprint">
-				<?php
-				/* translators: %s: WordPress */
-				printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' );
-				?>
-			</a>
-		</div><!-- .site-info -->
-	</footer><!-- .site-footer -->
+	<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link(
+			'<footer id="colophon" class="site-footer" role="contentinfo"><div class="site-info">',
+			'</div></footer>'
+		);
+	}
+	?>
 
 </div><!-- .site -->
 
